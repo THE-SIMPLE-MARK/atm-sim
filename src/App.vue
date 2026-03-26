ba1bd31f209a146730fcf46073d920f559f41f06
SIMPLE MARK <57036815+THE-SIMPLE-MARK@users.noreply.github.com>
2026-03-26 16:23:56 +0100

    chore: run formatting

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import { invoke } from "@tauri-apps/api/core";
+import { invoke } from "@tauri-apps/api/core"
+import { ref } from "vue"
 
-const greetMsg = ref<string | null>(null);
-const name = ref<string | null>(null);
+const greetMsg = ref<string | null>(null)
+const name = ref<string | null>(null)
 
 async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-  greetMsg.value = await invoke("greet", { name: name.value });
+	// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+	greetMsg.value = await invoke("greet", { name: name.value })
 }
 </script>
 
