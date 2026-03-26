611d60a2665ca1ffbba733875427f336c4c53536
SIMPLE MARK <57036815+THE-SIMPLE-MARK@users.noreply.github.com>
2026-03-26 17:13:32 +0100

    feat: disable default webview behaviors in prod

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -6,8 +6,12 @@ fn greet(name: &str) -> String {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-  tauri::Builder::default()
-    .plugin(tauri_plugin_opener::init())
+  let builder = tauri::Builder::default().plugin(tauri_plugin_opener::init());
+
+  #[cfg(not(feature = "debug"))]
+  let builder = builder.plugin(tauri_plugin_prevent_default::init());
+
+  builder
     .invoke_handler(tauri::generate_handler![greet])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
